8e5e9027508e35016905c7acf67500c64a915749
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-01-14 19:54:57 +0500

    title and icon changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import "./index.scss";
-import store from "./redux/store.js";
+// import store from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
   </React.StrictMode>
 );
